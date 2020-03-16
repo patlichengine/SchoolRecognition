89a643c3387c1e46182ac28297bb9e3e362d4a99
ODItina <primeunit2004@yahoo.com>
2020-03-16 08:14:35 +0100

    PinsCreateViewModel added 16-Mar-2020 08:14am

---
+++ SchoolRecognition/Models/PinsCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRecognition.Models
+{
+    public class PinsCreateViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid? RecognitionTypeId { get; set; }
+        public string SerialPin { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsInUse { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTime? DateCreated { get; set; }
+        ///
+        public int NoOfPinToGenerate { get; set; }
+    }
+}
