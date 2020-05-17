ee3377a5c737d445d9e644bbbe6c0e3974de9d8e
ODItina <primeunit2004@yahoo.com>
2020-05-17 09:07:47 +0100

    Manage location link to locationTypes, localGovernments and state views

--- SchoolRecognition/Models/OfficesViewDto.cs
+++ SchoolRecognition/Models/OfficesViewDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -39,6 +40,7 @@ namespace SchoolRecognition.Models
                 return stateAssigned;
             }
         }
+        [JsonIgnore]
         public virtual IEnumerable<OfficeStatesViewDto> OfficeStateOffices { get; set; }
         public virtual IEnumerable<SchoolsViewDto> OfficeSchools { get; set; }
     }
